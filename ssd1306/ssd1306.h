7ae252d8f3a4f7789934e6cac5838f45f32300f3
Aleksander Alekseev <afiskon@gmail.com>
2018-03-19 18:53:19 +0300

    Add repository URL to the ssd1306.h file

--- ssd1306/ssd1306.h
+++ ssd1306/ssd1306.h
@@ -1,6 +1,8 @@
 /**
  * This Library was originally written by Olivier Van den Eede (4ilo) in 2016.
  * Some refactoring was done and SPI support was added by Aleksander Alekseev (afiskon) in 2018.
+ *
+ * https://github.com/afiskon/stm32-ssd1306
  */
 
 #ifndef __SSD1306_H__
