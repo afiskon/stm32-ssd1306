c556ec9c1ecf76037947a4c8986997c826cd955b
Aleksander Alekseev <mail@eax.me>
2021-01-16 16:31:01 +0300

    s/LEDs/OLEDs

--- ssd1306/ssd1306_conf_template.h
+++ ssd1306/ssd1306_conf_template.h
@@ -46,7 +46,7 @@
 #define SSD1306_INCLUDE_FONT_11x18
 #define SSD1306_INCLUDE_FONT_16x26
 
-// Some LEDs don't display anything in first two columns.
+// Some OLEDs don't display anything in first two columns.
 // In this case change the following macro to 130.
 // The default value is 128.
 // #define SSD1306_WIDTH           130
