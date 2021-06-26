8a8d5356da5ce2b726312c6ebee848f5153264fa
Target Triple <TargetTriple@Outlook.com>
2021-06-27 00:07:58 +0800

    Simplified ssd1306_UpdateScreen().

--- ssd1306/ssd1306.c
+++ ssd1306/ssd1306.c
@@ -189,12 +189,12 @@ void ssd1306_UpdateScreen(void) {
     //  * 32px   ==  4 pages
     //  * 64px   ==  8 pages
     //  * 128px  ==  16 pages
-    for(uint8_t i = 0; i < SSD1306_HEIGHT/8; i++) {
-        ssd1306_WriteCommand(0xB0 + i); // Set the current RAM page address.
-        ssd1306_WriteCommand(0x00);
-        ssd1306_WriteCommand(0x10);
-        ssd1306_WriteData(&SSD1306_Buffer[SSD1306_WIDTH*i],SSD1306_WIDTH);
-    }
+#if defined(SSD1306_USE_SPI)
+    ssd1306_WriteCommand(0xB0); // Set the current RAM page address.
+    ssd1306_WriteCommand(0x00);
+    ssd1306_WriteCommand(0x10);
+#endif
+    ssd1306_WriteData(SSD1306_Buffer, SSD1306_BUFFER_SIZE);
 }
 
 //    Draw one pixel in the screenbuffer
