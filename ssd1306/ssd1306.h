8d31998322f57ca0da834f29ca0cc85824becbb0
chris goertz <chris.goertz@gmx.de>
2024-02-02 23:58:17 +0100

    fixed include

--- ssd1306/ssd1306.h
+++ ssd1306/ssd1306.h
@@ -1,6 +1,7 @@
 /**
  * This Library was originally written by Olivier Van den Eede (4ilo) in 2016.
- * Some refactoring was done and SPI support was added by Aleksander Alekseev (afiskon) in 2018.
+ * Some refactoring was done and SPI support was added by Aleksander Alekseev
+ * (afiskon) in 2018.
  *
  * https://github.com/afiskon/stm32-ssd1306
  */
@@ -8,13 +9,12 @@
 #ifndef __SSD1306_H__
 #define __SSD1306_H__
 
-#include <stddef.h>
 #include <_ansi.h>
+#include <stddef.h>
 
 _BEGIN_STD_C
 
-#include "ssd1306_conf.h"
-
+#include "ssd1306_conf_template.h"
 
 #if defined(STM32WB)
 #include "stm32wbxx_hal.h"
@@ -44,9 +44,9 @@ _BEGIN_STD_C
 #elif defined(STM32G4)
 #include "stm32g4xx_hal.h"
 #else
-#error "SSD1306 library was tested only on STM32F0, STM32F1, STM32F3, STM32F4, STM32F7, STM32L0, STM32L1, STM32L4, STM32H7, STM32G0, STM32G4, STM32WB MCU families. Please modify ssd1306.h if you know what you are doing. Also please send a pull request if it turns out the library works on other MCU's as well!"
+#error                                                                         \
+    "SSD1306 library was tested only on STM32F0, STM32F1, STM32F3, STM32F4, STM32F7, STM32L0, STM32L1, STM32L4, STM32H7, STM32G0, STM32G4, STM32WB MCU families. Please modify ssd1306.h if you know what you are doing. Also please send a pull request if it turns out the library works on other MCU's as well!"
 #endif
-
 
 #ifdef SSD1306_X_OFFSET
 #define SSD1306_X_OFFSET_LOWER (SSD1306_X_OFFSET & 0x0F)
@@ -61,11 +61,11 @@ _BEGIN_STD_C
 /* vvv I2C config vvv */
 
 #ifndef SSD1306_I2C_PORT
-#define SSD1306_I2C_PORT        hi2c1
+#define SSD1306_I2C_PORT hi2c1
 #endif
 
 #ifndef SSD1306_I2C_ADDR
-#define SSD1306_I2C_ADDR        (0x3C << 1)
+#define SSD1306_I2C_ADDR (0x3C << 1)
 #endif
 
 /* ^^^ I2C config ^^^ */
@@ -73,28 +73,28 @@ _BEGIN_STD_C
 /* vvv SPI config vvv */
 
 #ifndef SSD1306_SPI_PORT
-#define SSD1306_SPI_PORT        hspi2
+#define SSD1306_SPI_PORT hspi2
 #endif
 
 #ifndef SSD1306_CS_Port
-#define SSD1306_CS_Port         GPIOB
+#define SSD1306_CS_Port GPIOB
 #endif
 #ifndef SSD1306_CS_Pin
-#define SSD1306_CS_Pin          GPIO_PIN_12
+#define SSD1306_CS_Pin GPIO_PIN_12
 #endif
 
 #ifndef SSD1306_DC_Port
-#define SSD1306_DC_Port         GPIOB
+#define SSD1306_DC_Port GPIOB
 #endif
 #ifndef SSD1306_DC_Pin
-#define SSD1306_DC_Pin          GPIO_PIN_14
+#define SSD1306_DC_Pin GPIO_PIN_14
 #endif
 
 #ifndef SSD1306_Reset_Port
-#define SSD1306_Reset_Port      GPIOA
+#define SSD1306_Reset_Port GPIOA
 #endif
 #ifndef SSD1306_Reset_Pin
-#define SSD1306_Reset_Pin       GPIO_PIN_8
+#define SSD1306_Reset_Pin GPIO_PIN_8
 #endif
 
 /* ^^^ SPI config ^^^ */
@@ -109,40 +109,40 @@ extern SPI_HandleTypeDef SSD1306_SPI_PORT;
 
 // SSD1306 OLED height in pixels
 #ifndef SSD1306_HEIGHT
-#define SSD1306_HEIGHT          64
+#define SSD1306_HEIGHT 64
 #endif
 
 // SSD1306 width in pixels
 #ifndef SSD1306_WIDTH
-#define SSD1306_WIDTH           128
+#define SSD1306_WIDTH 128
 #endif
 
 #ifndef SSD1306_BUFFER_SIZE
-#define SSD1306_BUFFER_SIZE   SSD1306_WIDTH * SSD1306_HEIGHT / 8
+#define SSD1306_BUFFER_SIZE SSD1306_WIDTH *SSD1306_HEIGHT / 8
 #endif
 
 // Enumeration for screen colors
 typedef enum {
-    Black = 0x00, // Black color, no pixel
-    White = 0x01  // Pixel is set. Color depends on OLED
+  Black = 0x00, // Black color, no pixel
+  White = 0x01  // Pixel is set. Color depends on OLED
 } SSD1306_COLOR;
 
 typedef enum {
-    SSD1306_OK = 0x00,
-    SSD1306_ERR = 0x01  // Generic error.
+  SSD1306_OK = 0x00,
+  SSD1306_ERR = 0x01 // Generic error.
 } SSD1306_Error_t;
 
 // Struct to store transformations
 typedef struct {
-    uint16_t CurrentX;
-    uint16_t CurrentY;
-    uint8_t Initialized;
-    uint8_t DisplayOn;
+  uint16_t CurrentX;
+  uint16_t CurrentY;
+  uint8_t Initialized;
+  uint8_t DisplayOn;
 } SSD1306_t;
 
 typedef struct {
-    uint8_t x;
-    uint8_t y;
+  uint8_t x;
+  uint8_t y;
 } SSD1306_VERTEX;
 
 // Procedure definitions
@@ -151,17 +151,27 @@ void ssd1306_Fill(SSD1306_COLOR color);
 void ssd1306_UpdateScreen(void);
 void ssd1306_DrawPixel(uint8_t x, uint8_t y, SSD1306_COLOR color);
 char ssd1306_WriteChar(char ch, FontDef Font, SSD1306_COLOR color);
-char ssd1306_WriteString(char* str, FontDef Font, SSD1306_COLOR color);
+char ssd1306_WriteString(char *str, FontDef Font, SSD1306_COLOR color);
 void ssd1306_SetCursor(uint8_t x, uint8_t y);
-void ssd1306_Line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1306_COLOR color);
-void ssd1306_DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep, SSD1306_COLOR color);
-void ssd1306_DrawArcWithRadiusLine(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle, uint16_t sweep, SSD1306_COLOR color);
-void ssd1306_DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r, SSD1306_COLOR color);
-void ssd1306_FillCircle(uint8_t par_x,uint8_t par_y,uint8_t par_r,SSD1306_COLOR par_color);
-void ssd1306_Polyline(const SSD1306_VERTEX *par_vertex, uint16_t par_size, SSD1306_COLOR color);
-void ssd1306_DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1306_COLOR color);
-void ssd1306_FillRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, SSD1306_COLOR color);
-void ssd1306_DrawBitmap(uint8_t x, uint8_t y, const unsigned char* bitmap, uint8_t w, uint8_t h, SSD1306_COLOR color);
+void ssd1306_Line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2,
+                  SSD1306_COLOR color);
+void ssd1306_DrawArc(uint8_t x, uint8_t y, uint8_t radius, uint16_t start_angle,
+                     uint16_t sweep, SSD1306_COLOR color);
+void ssd1306_DrawArcWithRadiusLine(uint8_t x, uint8_t y, uint8_t radius,
+                                   uint16_t start_angle, uint16_t sweep,
+                                   SSD1306_COLOR color);
+void ssd1306_DrawCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r,
+                        SSD1306_COLOR color);
+void ssd1306_FillCircle(uint8_t par_x, uint8_t par_y, uint8_t par_r,
+                        SSD1306_COLOR par_color);
+void ssd1306_Polyline(const SSD1306_VERTEX *par_vertex, uint16_t par_size,
+                      SSD1306_COLOR color);
+void ssd1306_DrawRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2,
+                           SSD1306_COLOR color);
+void ssd1306_FillRectangle(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2,
+                           SSD1306_COLOR color);
+void ssd1306_DrawBitmap(uint8_t x, uint8_t y, const unsigned char *bitmap,
+                        uint8_t w, uint8_t h, SSD1306_COLOR color);
 
 /**
  * @brief Sets the contrast of the display.
@@ -187,8 +197,8 @@ uint8_t ssd1306_GetDisplayOn();
 // Low-level procedures
 void ssd1306_Reset(void);
 void ssd1306_WriteCommand(uint8_t byte);
-void ssd1306_WriteData(uint8_t* buffer, size_t buff_size);
-SSD1306_Error_t ssd1306_FillBuffer(uint8_t* buf, uint32_t len);
+void ssd1306_WriteData(uint8_t *buffer, size_t buff_size);
+SSD1306_Error_t ssd1306_FillBuffer(uint8_t *buf, uint32_t len);
 
 _END_STD_C
 
--- ssd1306/ssd1306_fonts.h
+++ ssd1306/ssd1306_fonts.h
@@ -3,12 +3,12 @@
 #ifndef __SSD1306_FONTS_H__
 #define __SSD1306_FONTS_H__
 
-#include "ssd1306_conf.h"
+#include "ssd1306_conf_template.h"
 
 typedef struct {
-	const uint8_t FontWidth;    /*!< Font width in pixels */
-	uint8_t FontHeight;   /*!< Font height in pixels */
-	const uint16_t *data; /*!< Pointer to data font data array */
+  const uint8_t FontWidth; /*!< Font width in pixels */
+  uint8_t FontHeight;      /*!< Font height in pixels */
+  const uint16_t *data;    /*!< Pointer to data font data array */
 } FontDef;
 
 #ifdef SSD1306_INCLUDE_FONT_6x8
