e702dd61457d355badb53f2eaa8ba32d68fb75ca
somesh-audioinventions <158574113+somesh-audioinventions@users.noreply.github.com>
2025-01-24 15:35:41 +0000

    changes to minimise disruption to original library

--- ssd1306/ssd1306_conf_template.h
+++ ssd1306/ssd1306_conf_template.h
@@ -7,7 +7,7 @@
 #define __SSD1306_CONF_H__
 
 // Choose a microcontroller family
-//#define STM32F0
+#define STM32F0
 //#define STM32F1
 //#define STM32F4
 //#define STM32L0
@@ -17,15 +17,15 @@
 //#define STM32H7
 //#define STM32F7
 //#define STM32G0
-#define STM32U5
+//#define STM32U5
 
 // Choose a bus
-//#define SSD1306_USE_I2C
+#define SSD1306_USE_I2C
 //#define SSD1306_USE_SPI
 
 // I2C Configuration
-//#define SSD1306_I2C_PORT        hi2c1
-//#define SSD1306_I2C_ADDR        (0x3C << 1)
+#define SSD1306_I2C_PORT        hi2c1
+#define SSD1306_I2C_ADDR        (0x3C << 1)
 
 // SPI Configuration
 //#define SSD1306_SPI_PORT        hspi1
